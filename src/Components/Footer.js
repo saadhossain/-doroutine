c9dcb5784fff4e4450149473ca7e1f1d393a4de0
Saad Hossain <mshsarkar@gmail.com>
2022-12-29 20:31:12 +0600

    Basic Footer Added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className='bg-secondary py-5 text-white font-poppins text-center'>
+            <p>&copy;2023 Allrights Reserved | Developed By <span className='text-actionbtn'><a href='https://saadhossain.dev/'>Saad Hossain</a></span></p>
         </div>
     );
 };
