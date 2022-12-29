622133a356c57202917538b3fa7e1832c4e962c8
Saad Hossain <mshsarkar@gmail.com>
2022-12-29 18:51:52 +0600

    Navbar responsive issue fixed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,12 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assests/DoRoutine-logo.png';
 import { AuthContext } from '../Context/AuthProvider';
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
 const Header = () => {
     const { user } = useContext(AuthContext)
+    const [expand, setExpand] = useState(false)
     return (
         <div className='bg-gray-50 border bottom-1 border-gray-100'>
             <div className='w-11/12 lg:w-10/12 mx-auto py-2 flex justify-between items-center'>
@@ -12,7 +14,7 @@ const Header = () => {
                     <img src={logo} alt='DoRoutine Logo' className='h-10' />
                 </Link>
                 <div className='flex gap-2 items-center'>
-                    <ul className='flex gap-2 font-semibold'>
+                    <ul className={`lg:flex gap-2 font-semibold font-bigshoulder absolute lg:static text-primary ${expand ? 'top-14 right-0': 'top-[-200px] right-0'} bg-gray-50 w-full py-2 px-5 shadow-lg duration-500 ease-in-out` }>
                         <li>
                             <Link to='/addtask'>Add Task</Link>
                         </li>
@@ -23,6 +25,11 @@ const Header = () => {
                             <Link>Completed Task</Link>
                         </li>
                     </ul>
+                    <div onClick={() => setExpand(!expand)} className='text-primary text-2xl lg:hidden'>
+                        {
+                            expand ? <AiOutlineClose></AiOutlineClose> : <AiOutlineMenu></AiOutlineMenu>
+                        }
+                    </div>
                     {
                         user
                             ? <Link to='/dashboard'>
