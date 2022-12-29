420e6353a35888b4aaeb80f5ab03640e922fa6d2
Saad Hossain <mshsarkar@gmail.com>
2022-12-30 02:25:11 +0600

    Navbar Dark mode optimized

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,13 +9,13 @@ const Header = () => {
     const { user, darkMode, setDarkMode } = useContext(AuthContext)
     const [expand, setExpand] = useState(false);
     return (
-        <div className='border-b border-gray-100'>
+        <div className={`border-b ${darkMode ? 'border-gray-300 bg-gray-100 ': 'border-primary bg-gray-900'}`}>
             <div className='w-11/12 lg:w-10/12 mx-auto py-2 flex justify-between items-center'>
                 <Link to='/'>
                     <img src={logo} alt='DoRoutine Logo' className='h-10' />
                 </Link>
                 <div className='flex gap-2 items-center'>
-                    <ul className={`lg:flex gap-5 font-semibold font-bigshoulder absolute lg:static text-primary ${expand ? 'top-14 right-0' : 'top-[-200px] right-0'} bg-gray-50 lg:bg-transparent w-full py-2 px-5 shadow-lg lg:shadow-none duration-500 ease-in-out`}>
+                    <ul className={`lg:flex gap-5 font-semibold font-bigshoulder absolute lg:static text-primary ${expand ? 'top-14 right-0' : 'top-[-200px] right-0'} ${darkMode ? 'bg-gray-50' : 'bg-gray-900'} lg:bg-transparent w-full py-2 px-5 shadow-xl lg:shadow-none duration-500 ease-in-out`}>
                         <li>
                             <Link to='/addtask'>Add Task</Link>
                         </li>
